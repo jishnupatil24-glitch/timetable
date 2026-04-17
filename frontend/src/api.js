689371aab90642e3c6ec65e8ad450e1e17c11dea
jishnupatil24-glitch <jishnupatil24@gmail.com>
2026-04-18 03:13:25 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
+const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8000"
+    : "https://timetable-real.onrender.com";
+
 const API = axios.create({
-    baseURL: 'http://localhost:8000',
+  baseURL: BASE_URL,
 });
 
 // Automatically attach token to every request
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
